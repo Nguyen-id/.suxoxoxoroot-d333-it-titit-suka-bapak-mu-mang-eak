b226ca912774a062274356f1dcaeee1cd6ab5025
proyek778 <145361899+proyek778@users.noreply.github.com>
2023-09-24 18:35:04 +0700

    Update surootbf.sh

--- surootbf.sh
+++ surootbf.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-help="
---------------HACKNCORP TOOLS--------------
-------BRUTE FORCE PASSWORD USER LINUX------
-----------------LUTFIFAKEE----------------
--------------COPYRIGHT - 2023-------------
+help="This tool bruteforces a selected user using binary su and as passwords: null password, username, reverse username and a wordlist (top12000.txt).
+You can specify a username using -u <username> and a wordlist via -w <wordlist>.
+By default the BF default speed is using 100 su processes at the same time (each su try last 0.7s and a new su try in 0.007s) ~ 143s to complete
+You can configure this times using -t (timeout su process) ans -s (sleep between 2 su processes).
+Fastest recommendation: -t 0.5 (minimun acceptable) and -s 0.003 ~ 108s to complete
 
-Example: ./surootbf.sh -u root [-w pass.txt] [-t 0.7] [-s 0.007]
+Example: ./suroot.sh -u root -w pass.txt -t 0.7 -s 0.007
 
-	Options:
-
-		-u	username
-		-w	wordlist password 
-		-t	threads [Kecepatan]
-		-s	sleep between 2 su processes
-
-USERNAME SENSITIF HURUF BESAR DAN SKRIP INI TIDAK MEMERIKSA APAKAH NAMA PENGGUNA YANG DIBERIKAN ADA, SEBAIKNYA CEK USERNAME TERLEBIH DAHULU
-
-Alat ini memaksa pengguna yang dipilih menggunakan su biner dan sebagai kata sandi: kata sandi nol, nama pengguna, nama pengguna terbalik, dan daftar kata (top12000.txt).
-Anda dapat menentukan nama pengguna menggunakan -u <nama pengguna> dan daftar kata melalui -w <daftar kata>.
-Secara default, kecepatan default BF menggunakan 100 proses su pada saat yang sama (setiap su percobaan berlangsung 0,7 detik dan su percobaan baru dalam 0,007 detik) ~ 143 detik hingga selesai
-Anda dapat mengonfigurasi waktu ini menggunakan -t (batas waktu proses su) dan -s (tidur di antara 2 proses su).
-Rekomendasi tercepat: -t 0,5 (minimun dapat diterima) dan -s 0,003 ~ 108 detik untuk diselesaikan\n\n"
-
+THE USERNAME IS CASE SENSITIVE AND THIS SCRIPT DOES NOT CHECK IF THE PROVIDED USERNAME EXIST, BE CAREFUL\n\n"
 
 WORDLIST="pass.txt"
 USER=""
@@ -55,8 +41,8 @@ su_try_pwd (){
 }
 
 su_brute_user_num (){
-  echo "
-       ________
+  echo " 
+       ________ 
       /    /   \
      /         /
     /         /
@@ -69,7 +55,8 @@ su_brute_user_num (){
 -------------COPYGRIHT - 2023-------------
 
 
-  [+] BRUTEFORCING USER : $1..."
+
+[+] Bruteforcing $1..."
   USER=$1
   su_try_pwd $USER "" &    #Try without password
   su_try_pwd $USER $USER & #Try username as password
